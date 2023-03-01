f8373eb25b0fd83fa0688348524439aa92c44ecc
Andrey Bodryagin <andrey.bodryagin.person@gmail.com>
2023-03-02 01:58:50 +0300

    add diffie_hellman and part of lab3

--- endpoint/main.py
+++ endpoint/main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI, status, Depends, Response
+from fastapi import FastAPI, status, Depends, Response, Header
 from fastapi.responses import JSONResponse
+from random import randint
 
 from usecase import register_user, get_code, auth_by_code
 from schemas.signUp import SignUpRequest
 from schemas.signIn import SignInRequest, CodeResponse, CodeRequest
 from exceptions import *
+from tools.diffie_hellman import get_primitive_root, get_safe_prime, fast_pow
 
 
 app = FastAPI()
 
+DATABASE_IN = {}
 
 
 @app.post(
@@ -44,3 +47,27 @@ async def signin(credentials: CodeRequest):
     except UserNotFound as err:
         return JSONResponse({'message': str(err)}, status_code=status.HTTP_400_BAD_REQUEST)
     return JSONResponse({'code': code}, status_code=status.HTTP_200_OK)
+
+
+
+@app.get('/get_diffie_params',
+         status_code=status.HTTP_200_OK,
+         response_class=Response)
+async def get_A(bit: int, user: str):
+        p = get_safe_prime(70)
+        g = get_primitive_root(70, p)
+        a = randint(10000, 10000)
+        A = fast_pow(g, a, p)
+        print(f'Created: p:{p} a:{a} g:{g}')
+        DATABASE_IN[user] = {'p': p, 'A': A, 'g': g, 'a': a}
+        return JSONResponse({'p': p, 'A': A, 'g': g}, status_code=status.HTTP_200_OK)
+
+
+@app.get('/get_user_param_diffie_hellman',
+         status_code=status.HTTP_200_OK,
+         response_class=Response)
+async def get_A(B: int, user: str):
+        data = DATABASE_IN[user]
+        k = fast_pow(B, data['a'], data['p'])
+        print(f'Server key = {k}')
+        DATABASE_IN[user]['k'] = k
